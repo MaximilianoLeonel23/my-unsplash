7427e6675c1d660d7715bdc08b90301806a6182a
Maximiliano Pérez <maximiliano.leonel.perez1999@gmail.com>
2023-08-08 10:58:27 -0300

    Reconfiguro firebase como al principio

--- config/firebase.ts
+++ config/firebase.ts
@@ -1,7 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import firebaseConfig from "./firebaseConfig";
 
-// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyApW6Qbfscn6-NntaWAW-LRyDvNiQ9uRro",
+  authDomain: "my-unsplash-21118.firebaseapp.com",
+  projectId: "my-unsplash-21118",
+  storageBucket: "my-unsplash-21118.appspot.com",
+  messagingSenderId: "821307001320",
+  appId: "1:821307001320:web:c0ae77b27676524ff37c5d",
+};
+
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
