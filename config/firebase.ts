13368c2f45b50ee805719b857b6f5d4dd83d64a4
Maximiliano Pérez <maximiliano.leonel.perez1999@gmail.com>
2023-08-08 10:56:14 -0300

    Cambio la importación de firebase

--- config/firebase.ts
+++ config/firebase.ts
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { firebaseConfig } from "./firebaseConfig";
+import firebaseConfig from "./firebaseConfig";
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
